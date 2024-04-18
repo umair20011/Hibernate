89a96da84d68272847923ea0bf0080781bce151c
Danish <danishmaniyar2110@gmail.com>
2024-04-18 23:03:42 +0530

    first commit

---
+++ src/test/java/com/danish/HibernateLearning/Alien.java
@@ -0,0 +1,27 @@
+package com.danish.HibernateLearning;
+
+public class Alien {
+    private int aid;
+    private String aname;
+    private String color;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+    
+    
+}
---
+++ src/test/java/com/danish/HibernateLearning/App.java
@@ -0,0 +1,15 @@
+package com.danish.HibernateLearning;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class App{
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
+	
