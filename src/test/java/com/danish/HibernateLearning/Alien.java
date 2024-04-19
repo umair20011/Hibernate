c02247b81d18dd5e5ccbed9ae88be53d504a79c9
Danish <danishmaniyar2110@gmail.com>
2024-04-19 16:20:58 +0530

    added some annotations to change the default table name used @Table annotation and to change the default column name used @Column annotation from jakarta.persistence

--- src/test/java/com/danish/HibernateLearning/Alien.java
+++ src/test/java/com/danish/HibernateLearning/Alien.java
@@ -1,13 +1,18 @@
 package com.danish.HibernateLearning;
 
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="alien_table")
 public class Alien {
 	@Id
     private int aid;
     private String aname;
+    @Column(name="alien_color")
     private String color;
 	public int getAid() {
 		return aid;
